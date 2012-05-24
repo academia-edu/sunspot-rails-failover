38dad54a11b7280b9eedb994019b9f2dcb57fbc8
Jure Triglav <juretriglav@gmail.com>
2012-05-24 23:26:58 +0200

    Yet another different approach.

--- lib/sunspot/session_proxy/master_slave_with_failover_session_proxy.rb
+++ lib/sunspot/session_proxy/master_slave_with_failover_session_proxy.rb
@@ -1,30 +1,34 @@
 module Sunspot
   module SessionProxy
-    class MasterSlaveWithFailoverSessionProxy < MasterSlaveSessionProxy  
+    class MasterSlaveWithFailoverSessionProxy < MasterSlaveSessionProxy
       attr_accessor :exception
-      
+
       def commit
         with_exception_handling do
           master_session.commit
         end
       end
-      
+
       def search(*types, &block)
         begin
           Timeout.timeout(0.01) do
             TCPSocket.new(slave_session.config.solr.url[7..-11], 'echo')
           end
-          result = with_exception_handling { slave_session.search(*types, &block) }
         rescue
-        end	
+          slave_dead = true
+        end
+
+        unless slave_dead
+          result = with_exception_handling { slave_session.search(*types, &block) }
+        end
         result ||= with_exception_handling { master_session.search(*types, &block) }
-        
+
         raise(exception) unless result
         result
       end
-      
+
     private
-    
+
       def with_exception_handling
         yield
       rescue Exception => exception
@@ -32,7 +36,7 @@ module Sunspot
         self.exception = exception
         false
       end
-      
+
     end
   end
 end
