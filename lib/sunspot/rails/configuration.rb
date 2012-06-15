8fc5a6a19ed14f6b71bfbb739f1fad1157b4f364
Jure Triglav <juretriglav@gmail.com>
2012-06-15 02:01:53 +0200

    Update with a less demanding regex match.

--- lib/sunspot/rails/configuration.rb
+++ lib/sunspot/rails/configuration.rb
@@ -6,7 +6,7 @@ module Sunspot
       attr_writer :user_configuration
 
       def method_missing(method_id, *arguments, &block)
-        if method_id.to_s =~ /^(?:master|slave)_([\w]+)_path$/
+        if method_id.to_s =~ /^(?:master_|slave_)?([\w]+)_path$/
           self.class.send :define_method, method_id do
             unless instance_variable_defined?("@#{method_id}")
               path = if has_master?
@@ -24,7 +24,7 @@ module Sunspot
       end
 
       def respond_to?(method_id, include_private = false)
-        if method_id.to_s =~ /^(?:master|slave)_([\w]+)_path$/
+        if method_id.to_s =~ /^(?:master_|slave_)?([\w]+)_path$/
           true
         else
           super
